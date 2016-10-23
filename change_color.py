3c890e396af873b96aeeaad58bb1d344024cf74d
Aleksandr <yavalvas@gmail.com>
2016-10-23 16:06:21 +0300

    fix parser

--- change_color.py
+++ change_color.py
@@ -1,23 +1,16 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from PIL import Image
-#import os
-#script_dir = os.path.dirname(os.path.abspath(__file__))
+
 def change_pic(path_pic):
     im = Image.open(path_pic)
     im = im.convert('RGBA')
     data = np.array(im)
-    # just use the rgb values for comparison
     rgb = data[:,:,:3]
-    color = [242, 242, 242]   # Original value
-    black = [0,0,0, 255]
+    color = [242, 242, 242]
     white = [255,255,255,255]
-    mask = np.all(rgb == color, axis = -1)
-    # change all pixels that match color to white
+    mask = np.all(rgb == color, axis=-1)
     data[mask] = white
-    # change all pixels that don't match color to black
-    ##data[np.logical_not(mask)] = black
     new_im = Image.fromarray(data)
-    print path_pic
-    # orig_color = (242,242,242)
+    print(path_pic)
     new_im.save(path_pic)
--- parser.py
+++ parser.py
@@ -1,22 +1,24 @@
 # !/usr/bin/local
 # -*- coding:utf-8 -*-
-import urllib
-from time import sleep as sl
+import urllib.request
 from random import randint
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import os
 import io
 from pymongo import MongoClient
 from sqlalchemy import create_engine, Table, Column, MetaData, String
 from change_color import change_pic
 import socket
+import inspect
 socket.setdefaulttimeout(2)
+
 
 def mongo_init():
     client = MongoClient('localhost', 27017)
     db = client.songs_database
     db.drop_collection("songs")
     return db
+
 
 def sqlite_init():
     db = create_engine('sqlite:///songs_singers.db')
@@ -35,6 +37,7 @@ def sqlite_init():
     songs.create()
     return songs
 
+
 class AmDmParser(object):
     def __init__(self, datatable_sqlite, datatable_mongo):
         self.songs_db = datatable_sqlite
@@ -47,7 +50,6 @@ class AmDmParser(object):
                                   "http://proxy-ip-list.com/download/free-uk-proxy-list.txt",
                                   "http://proxy-ip-list.com/download/free-proxy-list.txt"]
 
-
     def insert_info_mongodb(self, type_art, page_num, rus_name, eng_name, song_words, txt_path, url, song_name):
         song = {"type": type_art,
                 "page_num": page_num,
@@ -58,7 +60,7 @@ class AmDmParser(object):
                 "url": url,
                 "song_name": song_name}
         song_id = self.mongo_songs.insert(song)
-        print "ID песни", song_id
+        print("ID песни", song_id)
 
     def insert_info_sqlite(self, type_art, page_num, rus_name, eng_name, song_words, txt_path, url, song_name):
         song = {"type": type_art,
@@ -71,15 +73,15 @@ class AmDmParser(object):
                 "song_name": song_name}
         action = self.songs_db.insert()
         action.execute(song)
-        print "PK песни", url
+        print("PK песни", url)
 
     def set_proxies_list(self):
         self.candidate_proxies=[]
         for url_with_proxies in self.urls_with_proxies:
-            html = urllib.urlopen(url_with_proxies).read()
-            new_html = html.split("\r")
+            html = urllib.request.urlopen(url_with_proxies).read()
+            new_html = html.split(b"\r")
             other_list = new_html[3:-1]
-            self.candidate_proxies.extend([i.split(";")[0] for i in other_list])
+            self.candidate_proxies.extend([i.split(b";")[0] for i in other_list])
 
     def _download_data(self, href, path, song):
         while(1):
@@ -87,66 +89,72 @@ class AmDmParser(object):
                 self.set_proxies_list()
                 proxy = self.candidate_proxies[randint(0, len(self.candidate_proxies)-1)]
                 proxy={"https": proxy}
-                print "Trying HTTP proxy %s" % proxy
-                html = urllib.urlopen("http:"+href, proxies=proxy).read()
-                if "Too Many Requests" in html:
-                    print "Too Many Requests"
+                print("Trying HTTP proxy %s" % proxy)
+                self.set_opener(proxy)
+                html = urllib.request.urlopen("http:"+href).read()
+                if b"Too Many Requests" in html:
+                    print("Too Many Requests")
+                    continue
+                soup = BeautifulSoup(html, "lxml")
+                song_name = soup.find("span", {"itemprop": "name"})
+                song_words = str(soup.find("pre", {"itemprop": "chordsBlock"}))
+                song_applicature = str(soup.find("div", {"id": "song_chords"}))
+                if song_name is None:
                     continue
                 with io.open(path + song + u".txt", "w", encoding="utf-8") as trg:
-                    soup = BeautifulSoup(html)
-                    song_name = soup.find("span", {"itemprop": "name"})
-                    song_words = unicode(soup.find("pre", {"itemprop": "chordsBlock"}))
-                    song_applicature = unicode(soup.find("div", {"id": "song_chords"}))
-                    soup_for_chords = BeautifulSoup(song_applicature)
-                    # list_chords = [(tag["src"], tag["alt"]) if ")" not in tag["src"].split("/")[-1] else ("","")
-                    #                for tag in soup_for_chords.findAll("img")]
+                    soup_for_chords = BeautifulSoup(song_applicature, "lxml")
                     list_chords = [(tag["src"], tag["alt"]) for tag in soup_for_chords.findAll("img")]
-                    # print "PATH", os.path.realpath(__file__).replace("parser.py","")+path
-                    # os.chdir(os.path.realpath(__file__).replace("parser.py", "")+path)
                     directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-                    print "LIST CHORDS", list_chords
+                    print("LIST CHORDS", list_chords)
                     for url, chord_name in list_chords:
                         proxy = self.candidate_proxies[randint(0, len(self.candidate_proxies)-1)]
                         proxy = {"https": proxy}
-                        image = urllib.URLopener(proxies=proxy)
+                        image = urllib.request.URLopener(proxies=proxy)
                         filename = os.path.join(directory, chord_name)
                         try:
-                            image.retrieve(url, filename+".png")
+                            image.retrieve("http:"+url, filename+".png")
                         except IOError:
                             list_chords.remove((url, chord_name))
                             continue
-                        print filename
-                        change_pic((filename+".png").encode("utf-8"))
+                        print(filename)
+                        change_pic(filename+".png")
                     for string in song_words:
                         trg.write(string)
-                print "Got URL using proxy %s" % proxy
-                self.insert_info_mongodb(self.cur_dir.split("/")[-2], self.i, self.singer, self.singer_eng, song_words, path+song, href, song_name.getString())
-                self.insert_info_sqlite(self.cur_dir.split("/")[-2], self.i, self.singer, self.singer_eng, song_words, path+song, href, song_name.getString())
+                print("Got URL using proxy %s" % proxy)
+                self.insert_info_mongodb(self.cur_dir.split("/")[-2], self.i, self.singer, self.singer_eng, song_words, path+song, href, song_name.string)
+                self.insert_info_sqlite(self.cur_dir.split("/")[-2], self.i, self.singer, self.singer_eng, song_words, path+song, href, song_name.string)
                 break
-            except:
-                print "Trying next proxy in 5 seconds"
-                sl(0.001)
+            except Exception as e:
+                print("Trying next proxy", inspect.stack()[0][3])
 
     def _make_dir(self, href):
         dirs_list = href.split(u"/")[4:7]
-        dirs_list.pop(-2)
-        print dirs_list
+        try:
+            dirs_list.pop(-2)
+        except IndexError:
+            print("Pop index is out of range")
+        print(dirs_list)
         tmpPath = self.cur_dir+u"./"
         path = tmpPath+u"/".join(dirs_list)+"/"
         if not os.path.exists(path):
             os.makedirs(path)
             song_name = dirs_list[-1]
-            print path
+            print(path)
             self._download_data(href, path, song_name)
 
-
     def get_part_struct(self, elem):
-        print elem['href']
+        print(elem['href'])
         self._make_dir(elem['href'])
         return elem['href'], elem.string
+
     def get_full_struct(self, elem):
         return elem['href'], elem.string, \
-            map(self.get_part_struct, self.find_songs(elem['href'][2:])[:-1])
+            list(map(self.get_part_struct, self.find_songs(elem['href'][2:])[:-1]))
+
+    def set_opener(self, proxy):
+        proxy_support = urllib.request.ProxyHandler(proxy)
+        opener = urllib.request.build_opener(proxy_support)
+        urllib.request.install_opener(opener)
 
     def find_artists(self, url):
         while(1):
@@ -154,47 +162,45 @@ class AmDmParser(object):
                 self.set_proxies_list()
                 proxy = self.candidate_proxies[randint(0, len(self.candidate_proxies)-1)]
                 proxy = {"https": proxy}
-                print "Trying HTTP proxy %s" % proxy
-                html = urllib.urlopen("http://"+url, proxies=proxy).read()
-                if "Too Many Requests" in html:
-                        print "Too Many Requests"
-                        continue
-                print "Got URL using proxy %s" % proxy
-                soup = BeautifulSoup(html)
+                print("Trying HTTP proxy %s" % proxy)
+                self.set_opener(proxy)
+                html = urllib.request.urlopen(url).read()
+                if b"Too Many Requests" in html:
+                    print("Too Many Requests")
+                    continue
+                print("Got URL using proxy %s" % proxy)
+                soup = BeautifulSoup(html, "lxml")
                 artists = map(self.get_full_struct, soup.findAll('a',{'class':'artist'}))
-                return artists
-            except:
-                print "Trying next proxy in 5 seconds"
-                sl(0.001)
-
+                return list(artists)
+            except Exception as e:
+                print("Trying next proxy", inspect.stack()[0][3])
 
     def find_songs(self, url):
         while(1):
             try:
                 self.set_proxies_list()
                 proxy = self.candidate_proxies[randint(0, len(self.candidate_proxies)-1)]
-                print "Trying HTTP proxy %s" % proxy
+                print("Trying HTTP proxy %s" % proxy)
                 proxy = {"https": proxy}
-                html = urllib.urlopen(url, proxies=proxy).read()
-                if "Too Many Requests" in html:
-                    print "Too Many Requests"
+                self.set_opener(proxy)
+                html = urllib.request.urlopen("http://"+url).read()
+                if b"Too Many Requests" in html:
+                    print("Too Many Requests")
                     continue
-                print "Got URL using proxy %s" % proxy
-                soup = BeautifulSoup(html)
+                print("Got URL using proxy %s" % proxy)
+                soup = BeautifulSoup(html, "lxml")
                 songs = soup.findAll('a', {'class':'g-link'})
                 self.singer = soup.find('title').string.split(u"подборы")[0]
                 self.singer_eng = url.split("/")[-2]
-                print self.singer_eng
+                print(self.singer_eng)
                 return songs
-            except:
-                print "Trying next proxy in 5 seconds"
-                sl(0.001)
+            except Exception as e:
+                print("Trying next proxy", inspect.stack()[0][3])
+
     def get_arts_info(self, a, b, art_dict):
-        for self.i in xrange(a,b):
+        for self.i in range(a,b):
             cur_url = "http://www.amdm.ru/chords/%s"%self.i
-            #art_dict.update({self.i:self.find_artists(cur_url)})
             art_info=self.find_artists(cur_url)
-            # self.find_artists(cur_url)
         return art_info
 
     def get_one_art_info(self, cur_url = "http://www.amdm.ru/chords/0"):
@@ -206,16 +212,12 @@ if __name__ == '__main__':
     songs = songs_mongo.songs
     songs_db = sqlite_init()
     parser = AmDmParser(songs_db, songs)
-    
-    # parser.songs = songs_db
-    # parser.mongo_songs = songs
-    # rus_singers = parser.get_arts_info(1, 29, parser.cyr_art)
     parser.cur_dir = "./cyr/"
     rus_singers = parser.get_arts_info(1, 29, parser.cyr_art)
-    print rus_singers
+    print(rus_singers)
     parser.cur_dir = "./lat/"
     eng_singers = parser.get_arts_info(29, 55, parser.lat_art)
-    print eng_singers
+    print(eng_singers)
     parser.cur_dir = "./any/"
     anyone_singers = parser.get_one_art_info()
-    print anyone_singers
+    print(anyone_singers)
